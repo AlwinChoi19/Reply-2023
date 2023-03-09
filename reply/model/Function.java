b14e56915e487b06e7132420e68195e43de2ceeb
Oleksandr Nazymko <muvikonnn@gmail.com>
2023-03-09 19:32:01 +0100

    Add getDistance function

--- reply/model/Function.java
+++ reply/model/Function.java
@@ -1,5 +1,18 @@
 package reply.model;
+import java.util.ArrayList;
 
 public class Function {
 
+
+    public int getDistance(Coord x1,  ArrayList<Coord> Coords) {
+
+        int distance = 0;
+        for (Coord i : Coords) {
+            int temp = euklidDist(x1.getX(), i.getX(), x1.getY(), i.getY());
+            if (temp > distance)
+                distance = temp;
+        }
+
+        return distance;
+    }
 }
