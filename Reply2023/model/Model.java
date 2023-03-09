854f74cecb81f2a7a65c7e435b23aaffc9d246b6
AextraGit <wunderleaaron@gmail.com>
2023-03-09 17:20:23 +0100

    added Model n shit

--- Reply2023/model/Model.java
+++ Reply2023/model/Model.java
@@ -1,4 +1,21 @@
 
 public class Model {
-
+	private int[][] gameboard;
+	int gameboardSizeX, gameboardSizeY;
+	int currx;
+	int curry;
+	
+	public Model(int[][] gameboard, int gameboardSizeX, int gameboardSizeY){
+		this.gameboard = gameboard;
+		this.gameboardSizeX = gameboardSizeX;
+		this.gameboardSizeY = gameboardSizeY;
+	}
+	
+	public int[][] getHighestRatings{
+		for(int i = 0; i < gameboardSizeY)
+	}
+	
+	public int getValAtPos(int x, int y) {
+		return gameboard[x][y];
+	}
 }
