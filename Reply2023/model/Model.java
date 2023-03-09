ed2b072640ecd061606ad3fa98404b8ac516008e
AextraGit <wunderleaaron@gmail.com>
2023-03-09 17:38:48 +0100

    Added initial functions

--- Reply2023/model/Model.java
+++ Reply2023/model/Model.java
@@ -1,9 +1,9 @@
+import java.util.Collections;
 
 public class Model {
 	private int[][] gameboard;
 	int gameboardSizeX, gameboardSizeY;
-	int currx;
-	int curry;
+	Coord currCoord = new Coord;
 	
 	public Model(int[][] gameboard, int gameboardSizeX, int gameboardSizeY){
 		this.gameboard = gameboard;
@@ -11,8 +11,34 @@ public class Model {
 		this.gameboardSizeY = gameboardSizeY;
 	}
 	
-	public int[][] getHighestRatings{
-		for(int i = 0; i < gameboardSizeY)
+	public int[] getAvailableRatingsSorted() {
+		ArrayList<int> list = new ArrayList<>();
+		for(int i = 0; i < gameboardSizeX; i++) {
+			for(int j = 0; j < gameboardSizeY; j++) {
+				if(list.contains(gameboard[i][j])) {
+					continue;
+				}
+				list.add(gameboard[i][j]);
+			}
+		}
+		Collections.sort(list);
+		int[list.size()] arr = new int[];
+		for(int i = 0; i < list.size(); i++) {
+			arr[i] = list.get(i);
+		}
+		return arr;
+	}
+	
+	public Coord[] getRatings(int val){
+		ArrayList<Coord> list = new ArrayList<>();
+		for(int i = 0; i < gameboardSizeX) {
+			for(int j = 0; j < gameboardSizeY) {
+				if(gameboard[x][y] == val) {
+					list.add(new Coord(x,y));
+				}
+			}
+		}
+		return list.toArray(Coord[]::new);
 	}
 	
 	public int getValAtPos(int x, int y) {
