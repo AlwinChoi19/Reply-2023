c7f33612d7ddb6478ab26844e2fe2003db476378
AextraGit <wunderleaaron@gmail.com>
2023-03-09 18:22:53 +0100

    stuff for model

--- Reply2023/model/Model.java
+++ Reply2023/model/Model.java
@@ -47,9 +47,51 @@ public class Model {
 		return gameboard[x][y];
 	}
 	
+	public Coord getNearestCoordWithValue(Coord coord, int val) {
+		int[] wallsDist = new int[4];
+		/*
+		 * 0 => dist to left wall
+		 * 1 => dist to upper wall
+		 * 2 => dist to right Wall
+		 * 4 => dist to lower Wall
+		 */
+		wallsDist[0] = coord.getX();
+		wallsDist[1] = coord.getY();
+		wallsDist[2] = gameboardSizeX - coord.getX();
+		wallsDist[3] = gameboardSizeY - coord.getY();
+		Coord[] ratings = getRatings(val);
+		
+		for(int i = 0; i < getIndexOfSmallestVal(wallsDist); i++) {
+			
+		}
+		
+	}
+	
+	private int euklidDist(int x1, int x2, int y1, int y2) {
+		
+	}
+	
+	public int getIndexOfSmallestVal(int[] arr) {
+		int tmp = Integer.MAX_VALUE;
+		int tmpIndex = -1;
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] < tmp) {
+				tmp = arr[i];
+				tmpIndex = i;
+			}
+		}
+		return tmpIndex;
+	}
+	
 	public Coord getCoord(Coord currPos, int deltaX, int deltaY) {
+		int xPos = getXCoord(currPos, deltaX);
+		int yPos = getYCoord(currPos, deltaY);
+		
+		return new Coord(xPos, yPos);
+	}
+	
+	public int getXCoord(Coord currPos, int deltaX) {
 		int xPos = currPos.getX();
-		int yPos = currPos.getY();
 		
 		if(xPos + deltaX > gameboardSizeX - 1) {
 			xPos = xPos + deltaX % gameboardSizeX;
@@ -58,6 +100,11 @@ public class Model {
 		}else {
 			xPos += deltaX;
 		}
+		return xPos;
+	}
+	
+	public int getYCoord(Coord currPos, int deltaY) {
+		int yPos = currPos.getY();
 		
 		if(yPos + deltaY > gameboardSizeY - 1) {
 			yPos = (yPos + deltaY) % gameboardSizeX;
@@ -66,6 +113,6 @@ public class Model {
 		}else {
 			yPos += deltaY;
 		}
-		return new Coord(xPos, yPos);
+		
 	}
 }
